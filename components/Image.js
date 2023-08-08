dcf18d0d00fd34257be1b728818ff050f43c901c
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2023-08-08 15:58:18 -0400

    maxWidth

--- components/Image.js
+++ components/Image.js
@@ -6,7 +6,8 @@ const Image = ({ src, alt }) => (
   <img
     src={src} alt={alt}
     style={{
-      maxHeight: "33vh",
+      maxWidth: "100%",
+      maxHeight: "50vh",
       // maxWidth: "calc(100vw - 32px)",
       objectFit: "contain",
       borderRadius: "8px",
