d2e3964002ead7b3ce1c0db7fb28bd81d4db2067
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2023-08-09 13:28:23 -0400

    whole dropdown fix

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -9,8 +9,8 @@ export default function Dropdown({ title, children }) {
   const toggleOpen = () => setIsOpen(!isOpen);
 
   return (
-    <div style={{marginBottom: "8px"}}>
-      <Box onClick={toggleOpen} sx={{alignItems: "center",  cursor: "pointer", display: "flex", padding: '8px', borderRadius: "32px", border: "#e0e6ed 1px solid", backgroundColor: "#fff"}}>
+    <div  onClick={toggleOpen} style={{marginBottom: "8px"}}>
+      <Box sx={{alignItems: "center",  cursor: "pointer", display: "flex", padding: '8px', borderRadius: "32px", border: "#e0e6ed 1px solid", backgroundColor: "#fff"}}>
         <Icon style={{marginLeft: "8px", marginRight: "8px", flexShrink:0}} glyph={!isOpen ? "down-caret" : "up-caret"}/>
         {title}
       </Box>
