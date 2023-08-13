aeec67617ef77babd945926ebb1bb33dc778964e
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-08-13 10:39:28 -0700

    add jam easter egg

--- components/Header.js
+++ components/Header.js
@@ -49,10 +49,65 @@ export default function Header({
     }
   }, [])
 
-  const easterEgg = (event) => {
+  const easterEgg = event => {
     // If enter key and input value is "raspberry jam"
-    if (event.keyCode === 13 && event.target.value === "raspberry jam") {
-      window.open("https://example.com", "blank")
+    if (
+      event.keyCode === 13 &&
+      event.target.value.toLowerCase() === 'raspberry jam'
+    ) {
+      setQuery('')
+      // For now it just opens the gist in a new tab, in the future it would be cool to have like a modal pop up or something
+      window.open(
+        'https://gist.githubusercontent.com/NalinPlad/a07b5ade2b557d47513bf9306aeacd19/raw/f96049b05c6ec249bc55dad18c6f6b3808379dbd/raspberryjam.recipe',
+        'blank'
+      )
+
+      alert('[===RASPBERRY=JAM=MODE=ACTIVATED===]')
+
+      setInterval(_ => {
+        document.querySelectorAll('img').forEach(img => {
+          // hack club banner thing at top breaks it so dont change that image
+          if (img.parentElement.href != 'https://hackclub.com/') {
+            img.src = 'jam.jpeg'
+          }
+        })
+
+        document.querySelectorAll('canvas').forEach(canvas => {
+          // fill canvas with raspberry jam image without using Image()
+          const ctx = canvas.getContext('2d')
+          ctx.drawImage(
+            document.querySelector('img'),
+            0,
+            0,
+            canvas.width,
+            canvas.height
+          )
+        })
+      }, 100)
+
+      // document.querySelectorAll("p, span, h1, h2, h3, h4, h5, h6").forEach((el) => {
+      //   el.innerText = "I love JAM";
+      //   setTimeout(_ => {
+
+      //     el.animate(
+      //       [
+      //         // keyframes
+      //         { transform: "rotate(1deg)" },
+      //         {
+      //           transform: "rotate(-1deg)" ,
+      //           color: "tomato"
+      //         },
+      //         { transform: "rotate(1deg)" },
+      //       ],
+      //       {
+      //         // timing options
+      //         duration: 1000,
+      //         iterations: Infinity,
+      //         easing: "ease-in-out"
+      //       }
+      //     );
+      //   }, Math.random * 1000)
+      // });
     }
   }
 
