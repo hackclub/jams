e83124e1077682a39706a1bb9c84ecb0a6150876
Astro <114089666+astro-develops@users.noreply.github.com>
2024-09-06 23:33:27 +0000

    fixing pr 223

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,8 @@
 export default function NotFoundPage() {
-  return <h1>That's a 404!</h1>
+  return (
+    <>
+      <h1 style={{margin:"2rem"}}>That's a 404!</h1>
+      <a href="/">Go back home</a>
+    </>
+  )
 }
