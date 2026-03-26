aa7a44854bafc09df221931dd84676cfc6f1cea8
Echo <github@3kh0.net>
2026-03-26 09:56:47 -0400

    eslint + prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import prettier from 'eslint-config-prettier/flat'
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  prettier,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+])
+
+export default eslintConfig
