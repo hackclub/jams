70d9b24c44f64e317f78799e702288085f763cee
Kai Pereira <kaipereira2020@gmail.com>
2026-07-12 02:46:22 -0700

    whoopsies, just removing a log I left in

--- pages/guides/index.js
+++ pages/guides/index.js
@@ -48,8 +48,6 @@ export default function Index(props) {
     setFilteredGuides(filtered);
   }, [query, selectedCategories, difficulty, time]);
 
-  console.log(difficulty);
-
   return (
     <>
         <Header
